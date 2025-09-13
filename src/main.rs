e010f76e52d3d102e6719661571e184e470461e6
Peter Schier-Thaler <schierpeter@hotmail.com>
2025-09-13 15:00:34 +0200

    added VideoControls bar

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,7 @@ use std::error::Error;
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ui = AppWindow::new()?;
-
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
+    let ui = App::new()?;
 
     ui.run()?;
 
